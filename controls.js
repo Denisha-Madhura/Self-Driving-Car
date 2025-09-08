fa904e2ba711f057966afa445d458ed56a12c271
Denisha-Madhura <denisha.madhura2007@gmail.com>
2025-09-08 13:07:02 +0530

    added traffic, collision

--- controls.js
+++ controls.js
@@ -1,14 +1,21 @@
 class Controls{
-    constructor(){
+    constructor(type){
         this.forward=false;
         this.left=false;
         this.right=false;
         this.reverse=false;
 
-        this.#addKeyboardListeners();
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
     }
 
-    #addKeyboardListeners(){  // private method since it starts with #
+    #addKeyboardListeners(){
         document.onkeydown=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
@@ -25,7 +32,6 @@ class Controls{
                     break;
             }
         }
-
         document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
